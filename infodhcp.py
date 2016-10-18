7399b43eca861f7202ce09ea1f3fa1fd14e0284d
Juan Luis Ramirez Vaquero <juanlu@Juanlu-PC>
2016-10-18 09:46:54 +0200

    Modificado fichero infodhcp.py

--- infodhcp.py
+++ infodhcp.py
@@ -7,9 +7,9 @@ if sys.argv[1] == "-l":
 	concesiones_clientes = commands.getoutput("cat /var/lib/dhcp/dhcpd.leases |grep '{' |uniq |awk '{print $2}'")
 	reservas_IP = commands.getoutput("cat /etc/dhcp/dhcpd.conf |grep 'fixed-address' |sort |uniq|awk '{print $2}'")
 	reservas_IP = reservas_IP.replace(";","")
-	print "Las direcciones IP que ha concedido el servidor DHCP son: /n"
+	print "Las direcciones IP que ha concedido el servidor DHCP son: \n"
 	print concesiones_clientes
-	print "Las direcciones IP que se han reservador son: /n"
+	print "Las direcciones IP que se han reservador son: \n"
 	print reservas_IP
 
 else: 
@@ -17,6 +17,6 @@ else:
 	concesiones_clientes = concesiones_clientes.replace(";", "");
 	if len(concesiones_clientes) > 0:
 		print "IP;",sys.argv[1]
-		print "MAC:",concesion
+		print "MAC:",concesiones_clientes
 	else:
 		print "No hay ninguna concesión con la ip %s " % sys.argv[1] 
